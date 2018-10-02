38f3c9c28adbf84eeafcd359e2d4ee84663ee8b4
Austin Fouch <afouch2017@gmail.com>
2018-10-01 21:57:29 -0400

    Implemented GameActivity.OnClick(). Adds listener to every ImageView, except currentTileView. Added tags to each ImageView in boardLayout in order to grab position data, i.e. tileView.getTag(0) returns row #, and tileView.getTag(1) returns col #. Clicking any tile will create a DialogAlert prompt with the tile's position data as the message.

--- app/src/main/java/austinfouch/com/ishido/GameActivity.java
+++ app/src/main/java/austinfouch/com/ishido/GameActivity.java
@@ -1,17 +1,22 @@
 package austinfouch.com.ishido;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.media.Image;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 // TODO: DOC
-public class GameActivity extends AppCompatActivity
+public class GameActivity extends AppCompatActivity implements View.OnClickListener
 {
 
     @Override
@@ -34,5 +39,48 @@ public class GameActivity extends AppCompatActivity
         Game game = new Game(this);
         game.setup(boardLayout, player1NameLayout, player1ScoreLayout, player2NameLayout,
                 player2ScoreLayout, currTileLayout, tileCountLayout);
+
+        // TODO: HELP, QUIT listeners
+
+        for( Integer i = 0; i < boardLayout.getChildCount(); i++)
+        {
+            View rowView = boardLayout.getChildAt(i);
+            if( rowView instanceof TableRow)
+            {
+                for( Integer j = 0; j < ((TableRow) rowView).getChildCount(); j++)
+                {
+                    View tileView = ((TableRow) rowView).getChildAt(j);
+                    if ( tileView instanceof ImageView)
+                    {
+                        tileView.setTag(0, i);
+                        tileView.setTag(1, j);
+                        tileView.setOnClickListener(this);
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        ImageView currTileLayout = (ImageView) findViewById(R.id.currentTileView);
+        int currTileId = currTileLayout.getId();
+        if(v instanceof ImageView && v.getId() != currTileId) // is ImageView, but not currentTileView
+        {
+            new AlertDialog.Builder(this)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .setTitle("Confirming Tile Placement")
+                    .setMessage("row:" + v.getTag(0).toString() + " col:" + v.getTag(1).toString() )
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener()
+                    {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+
+                    })
+                    .setNegativeButton("No", null)
+                    .show();
+        }
     }
 }
